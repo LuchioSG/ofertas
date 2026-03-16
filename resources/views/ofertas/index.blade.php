7b8f5208dbb6d7ff391d523be59906a687c64654
Luciano Gaytan <luciano.gaytan7375@alumnos.udg.mx>
2026-03-15 18:13:47 -0600

    metodo index

---
+++ resources/views/ofertas/index.blade.php
@@ -0,0 +1,29 @@
+<h1>Lista de ofertas</h1>
+
+<a href="{{ route('ofertas.create') }}">Nueva oferta</a>
+
+<table border="1">
+<tr>
+<th>Titulo</th>
+<th>Tienda</th>
+<th>Acciones</th>
+</tr>
+
+@foreach($ofertas as $oferta)
+<tr>
+<td>{{ $oferta->titulo }}</td>
+<td>{{ $oferta->tienda }}</td>
+<td>
+<a href="{{ route('ofertas.show',$oferta->id) }}">Ver</a>
+<a href="{{ route('ofertas.edit',$oferta->id) }}">Editar</a>
+
+<form action="{{ route('ofertas.destroy',$oferta->id) }}" method="POST">
+@csrf
+@method('DELETE')
+<button type="submit">Eliminar</button>
+</form>
+
+</td>
+</tr>
+@endforeach
+</table>
